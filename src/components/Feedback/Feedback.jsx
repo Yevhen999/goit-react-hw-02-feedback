0d1df299b4e48f9926645a9cc8432ea51fddbc64
Yevhen <leshchenkoyevhen@gmail.com>
2022-11-19 20:42:11 -0500

    add styles to buttons

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -9,37 +9,44 @@ class Feedback extends Component {
     bad: 0,
   };
 
+  handleIncrement = event => {
+    console.log('increase');
+    setTimeout(() => {
+      console.log(event.target);
+    }, 1000);
+  };
+
   render() {
     return (
-      <div>
+      <div className={css.feedbackWrapper}>
         <h1>Please leave feedback</h1>
-        <button
-          className={css.btnFeedback}
-          type="button"
-          onClick={() => {
-            console.log('Клік');
-          }}
-        >
-          Good
-        </button>
-        <button
-          className={css.btnFeedback}
-          type="button"
-          onClick={() => {
-            console.log('Клік');
-          }}
-        >
-          Neutral
-        </button>
-        <button
-          className={css.btnFeedback}
-          type="button"
-          onClick={() => {
-            console.log('Клік');
-          }}
-        >
-          Bad
-        </button>
+        <div className={css.btnWrapper}>
+          <button
+            className={css.btnFeedback}
+            type="button"
+            onClick={this.handleIncrement}
+          >
+            Good
+          </button>
+          <button
+            className={css.btnFeedback}
+            type="button"
+            onClick={() => {
+              console.log('Клік');
+            }}
+          >
+            Neutral
+          </button>
+          <button
+            className={css.btnFeedback}
+            type="button"
+            onClick={() => {
+              console.log('Клік');
+            }}
+          >
+            Bad
+          </button>
+        </div>
       </div>
     );
   }
