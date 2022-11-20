b173f422be144241b4e99f5bb9618020b2229c96
Yevhen <leshchenkoyevhen@gmail.com>
2022-11-19 21:04:44 -0500

    add statistics

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,24 +2,26 @@ import { Component } from 'react';
 import css from './Feedback.module.css';
 
 class Feedback extends Component {
-  super() {}
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+  constructor() {
+    super();
+    this.state = {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
+  }
 
   handleIncrement = event => {
     console.log('increase');
     setTimeout(() => {
-      console.log(event.target);
+      console.log(event.target.textContent);
     }, 1000);
   };
 
   render() {
     return (
       <div className={css.feedbackWrapper}>
-        <h1>Please leave feedback</h1>
+        <h2>Please leave feedback</h2>
         <div className={css.btnWrapper}>
           <button
             className={css.btnFeedback}
@@ -47,6 +49,10 @@ class Feedback extends Component {
             Bad
           </button>
         </div>
+        <h2>Statistics</h2>
+        <p className={css.statisticsCount}>Good:</p>
+        <p className={css.statisticsCount}>Neutral:</p>
+        <p className={css.statisticsCount}>Bad:</p>
       </div>
     );
   }
