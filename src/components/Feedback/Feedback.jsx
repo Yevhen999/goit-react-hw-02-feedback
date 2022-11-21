c2eedef891194150a0ec52fc3e7fd11f6ed28b51
Yevhen <leshchenkoyevhen@gmail.com>
2022-11-21 14:24:41 -0500

    add counter good feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -2,20 +2,16 @@ import { Component } from 'react';
 import css from './Feedback.module.css';
 
 class Feedback extends Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  handleIncrement = () => {
+    console.log(this.state.good);
 
-  handleIncrement = event => {
-    console.log('increase');
-    setTimeout(() => {
-      console.log(event.target.textContent);
-    }, 1000);
+    this.setState({ good: (this.state.good += 1) });
+    console.log(this.state.good);
   };
 
   render() {
@@ -50,9 +46,9 @@ class Feedback extends Component {
           </button>
         </div>
         <h2>Statistics</h2>
-        <p className={css.statisticsCount}>Good:</p>
-        <p className={css.statisticsCount}>Neutral:</p>
-        <p className={css.statisticsCount}>Bad:</p>
+        <p className={css.statisticsCount}>Good: {this.state.good}</p>
+        <p className={css.statisticsCount}>Neutral: {this.state.neutral}</p>
+        <p className={css.statisticsCount}>Bad: {this.state.bad}</p>
       </div>
     );
   }
